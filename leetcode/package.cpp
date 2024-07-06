f02d537e5529b55f3c21ea1ec5065193bcc825ab
Sulaiman <f22dm0486@student.nafa.edu.sg>
2024-07-06 22:13:58 +0700

    fetch new

---
+++ leetcode/package.cpp
@@ -0,0 +1,20 @@
+{
+  "scripts": {
+    "prepare": "husky install"
+  },
+  "devDependencies": {
+    "@commitlint/cli": "^17.6.5",
+    "@commitlint/config-conventional": "^17.6.5",
+    "@prettier/plugin-php": "^0.19.6",
+    "clang-format": "1.8.0",
+    "husky": "^8.0.3",
+    "lint-staged": "^13.2.2",
+    "prettier": "^2.8.8",
+    "prettier-plugin-rust": "^0.1.9",
+    "prettier-plugin-sql-cst": "^0.10.0"
+  },
+  "lint-staged": {
+    "*.{js,ts,php,sql,rs,md}": "prettier --write",
+    "*.py": "black -S"
+  }
+}
